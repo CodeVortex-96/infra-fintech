2e64e553b91c269f7bb281ea0627de1262780620
CodeVortex-96 <jesusjoaopaulo438@gmail.com>
2026-07-03 07:58:53 -0300

    🚀 [GIT-OPS] Sincronização completa: app, infra e pipeline

--- terraform/modules/rede/variables.tf
+++ terraform/modules/rede/variables.tf
@@ -3,7 +3,6 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
   
-  # Aqui entra o nível Tech Lead: Validação
   validation {
     condition     = can(cidrhost(var.vpc_cidr, 0))
     error_message = "O CIDR informado não é um formato de bloco IP válido."
@@ -15,9 +14,25 @@ variable "env" {
   type        = string
   default     = "production"
 
-  # Validação de lista permitida
   validation {
     condition     = contains(["production", "staging", "dev"], var.env)
     error_message = "O ambiente deve ser 'production', 'staging' ou 'dev'."
   }
+}
+
+# Variáveis adicionadas para resolver o erro de Unsupported Argument
+variable "public_subnet_cidr" {
+  description = "CIDR da subnet pública"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR da subnet privada"
+  type        = string
+}
+
+variable "region" {
+  description = "Região da AWS"
+  type        = string
+  default     = "us-east-1"
 }
